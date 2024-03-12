8875ab9d7b4cc723cf97001a8fd47a6aecb66a22
marcoojea <marcoojea97@gmail.com>
2024-03-12 12:05:15 +0100

    segundo commit

--- src/main/java/org/example/Auto.java
+++ src/main/java/org/example/Auto.java
@@ -2,40 +2,66 @@ package org.example;
 
 import java.util.ArrayList;
 
-
+    /**
+     * Dentro de la clase auto tenemos las clases marca y modelo
+     * @author Marco Ojea
+     * @#version 1.0
+     */
 public class Auto {
-
+    //Atributos de la clase auto
 
     private String marca;
     private String modelo;
 
 
+        /**
+         * Constructor de auto
+         * @param marca del coche (auto)
+         * @param modelo del coche (auto)
+         */
     public Auto(String marca, String modelo) {
         this.marca = marca;
         this.modelo = modelo;
     }
 
+        /**
+         * Metodo que devuelve la marca del coche (auto)
+         * @return marca
+         */
 
     public String getMarca() {
         return marca;
     }
 
-
+        /**
+         * Metodo que actualiza la marca actual
+         * @param marca del coche actualizada (auto)
+         */
     public void setMarca(String marca) {
         this.marca = marca;
     }
 
-
+        /**
+         * Metodo que define el modelo del auto
+         * @return modelo del coche (auto)
+         */
     public String getModelo() {
         return modelo;
     }
 
-
+        /**
+         * Metodo que actualiza el modelo del coche (auto)
+         * @param modelo del auto
+         */
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
 
+        /**
+         * Metodo que nos convierte un objeto de la clase Auto a un objeto de tipo String
+         * @return cadena que representa los valores del Auto
+         */
     @Override
     public String toString() {
         return "Auto [marca=" + marca + ", modelo=" + modelo + "]";
--- src/main/java/org/example/Concesionario.java
+++ src/main/java/org/example/Concesionario.java
@@ -1,26 +1,44 @@
 package org.example;
 
 import java.util.ArrayList;
-
+    /**
+     * Clase concesionario
+     * @author Marco Ojea
+     * @version 1.0
+     */
 public class Concesionario {
-
-
+    /**
+     * ArrayList o Lista de los autos (coches)
+     */
     private ArrayList<Auto> autos;
+
+        /**
+         * Constructor de la lista de concesionarios
+         */
     public Concesionario() {
         autos = new ArrayList<>();
     }
 
+        /**
+         *
+         * @param auto añadimos un coche a la lista de concesionario
+         */
 
     public void agregarAuto(Auto auto) {
         autos.add(auto);
     }
 
-
+        /**
+         *
+         * @return La lista de los coches actualizada
+         */
     public ArrayList<Auto> listarAutos() {
         return autos;
     }
 
-
+        /**
+         * Imprime los coches de la lista de coches
+         */
     public void imprimirAutos(){
         for (Auto auto: autos){
             System.out.println(auto);
