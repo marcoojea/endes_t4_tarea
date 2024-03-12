56bea6d4e014176ef25aa399fbdfebec3f10fc91
marcoojea <marcoojea97@gmail.com>
2024-03-12 12:12:33 +0100

    JavaDoc

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Auto"},{"p":"org.example","l":"Concesionario"},{"p":"org.example","l":"Main"}];updateSearchResults();
